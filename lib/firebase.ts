1c49f5f053ae3ae53000567be41220b4396b4293
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-04-19 17:02:47 +0200

    init firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,26 @@
+import admin, { ServiceAccount } from "firebase-admin";
+
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
+
+const databaseURL = process.env.FIREBASE_DATABASE_URL;
+
+export function getFirebaseAdmin() {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL,
+    });
+  }
+
+  return admin;
+}
+
+const firebaseAdmin = getFirebaseAdmin();
+
+const db = firebaseAdmin.firestore();
+
+export { firebaseAdmin, db };
