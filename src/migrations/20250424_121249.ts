58d86d668ab22b14ea78de6c69ef4d5bc3b0adda
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-04-24 14:13:16 +0200

    add unique index migration

---
+++ src/migrations/20250424_121249.ts
@@ -0,0 +1,11 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   CREATE UNIQUE INDEX IF NOT EXISTS "competitions_slug_idx" ON "competitions" USING btree ("slug");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   DROP INDEX IF EXISTS "competitions_slug_idx";`)
+}
