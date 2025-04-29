017b271d159d9eecf92520f9abbca5e0fe412ea3
riccardo.tornesello <riccardo.tornesello@gmail.com>
2025-04-29 11:54:03 +0200

    fix time format

--- lib/format.ts
+++ lib/format.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 
 export function formatMilliseconds(totalMilliseconds: number): string {
   const durationObj = dayjs.duration(Math.floor(totalMilliseconds));
-  return durationObj.format("mm:ss.SSS");
+  return durationObj.format("m:ss.SSS");
 }
 
 export function formatDate(ts: string): string {
