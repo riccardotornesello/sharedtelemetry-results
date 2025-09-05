f85719a1f4f7b3bfaa13e035ea8859723e56517f
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-09-05 10:38:53 +0200

    split ranking by category

--- features/ranking/components/ranking-table.tsx
+++ features/ranking/components/ranking-table.tsx
@@ -1,8 +1,8 @@
-import dayjs from "dayjs"
 import { Competition } from "@/payload-types"
 import { RankingItem } from "../types"
 import { formatDate } from "@/lib/format"
 import { TimeCard } from "@/components/time-card"
+import { Fragment } from "react"
 
 export interface RankingTableProps {
   competition: Competition
@@ -20,6 +20,7 @@ interface RankingRowProps {
   team: NonNullable<Competition["teams"]>[number]
   eventGroups: EventGroup[]
   overallBestTimesPerEventGroup: Record<string, number>
+  position: number
 }
 
 export function RankingTable({ competition, ranking }: RankingTableProps) {
@@ -28,8 +29,8 @@ export function RankingTable({ competition, ranking }: RankingTableProps) {
   }
 
   const driversMap: Record<number, CompetitionDriver> = {}
+  const driversCrewsMap: Record<number, CompetitionCrew> = {}
   const driversTeamsMap: Record<number, CompetitionTeam> = {}
-  const overallBestTimesPerEventGroup: Record<string, number> = {}
 
   ;(competition.teams || []).forEach((team) => {
     ;(team.crews || []).forEach((crew) => {
@@ -39,21 +40,8 @@ export function RankingTable({ competition, ranking }: RankingTableProps) {
         }
 
         driversMap[driver.iRacingId] = driver
+        driversCrewsMap[driver.iRacingId] = crew
         driversTeamsMap[driver.iRacingId] = team
-      })
-    })
-  })
-
-  ranking.forEach((driverRanking) => {
-    Object.entries(driverRanking.results).forEach(([eventGroupId, results]) => {
-      Object.values(results).forEach((result) => {
-        if (
-          result > 0 &&
-          (!overallBestTimesPerEventGroup[eventGroupId] ||
-            result < overallBestTimesPerEventGroup[eventGroupId])
-        ) {
-          overallBestTimesPerEventGroup[eventGroupId] = result
-        }
       })
     })
   })
@@ -95,18 +83,152 @@ export function RankingTable({ competition, ranking }: RankingTableProps) {
       </thead>
 
       <tbody>
-        {ranking.map((rankingItem, i) => (
+        <TableBody
+          ranking={ranking}
+          driversMap={driversMap}
+          driversCrewsMap={driversCrewsMap}
+          driversTeamsMap={driversTeamsMap}
+          competition={competition}
+        />
+      </tbody>
+    </table>
+  )
+}
+
+function TableBody({
+  ranking,
+  driversMap,
+  driversCrewsMap,
+  driversTeamsMap,
+  competition,
+}: {
+  ranking: RankingItem[]
+  driversMap: Record<number, CompetitionDriver>
+  driversCrewsMap: Record<number, CompetitionCrew>
+  driversTeamsMap: Record<number, CompetitionTeam>
+  competition: Competition
+}) {
+  const classes = competition?.classes || []
+
+  // Multiclass
+  if (classes.length > 1) {
+    // class / event group / best time
+    const overallBestTimesPerEventGroup: Record<
+      string,
+      Record<string, number>
+    > = {}
+
+    classes.forEach((c) => {
+      overallBestTimesPerEventGroup[c.name] = {}
+    })
+
+    ranking.forEach((driverRanking) => {
+      Object.entries(driverRanking.results).forEach(
+        ([eventGroupId, results]) => {
+          Object.values(results).forEach((result) => {
+            const crew = driversCrewsMap[driverRanking.custId]
+            const crewClass = crew?.class || "-"
+
+            if (
+              result > 0 &&
+              (!overallBestTimesPerEventGroup[crewClass][eventGroupId] ||
+                result < overallBestTimesPerEventGroup[crewClass][eventGroupId])
+            ) {
+              overallBestTimesPerEventGroup[crewClass][eventGroupId] = result
+            }
+          })
+        }
+      )
+    })
+
+    const rankingPerClass: Record<string, RankingItem[]> = classes.reduce(
+      (acc, c) => {
+        acc[c.name] = ranking.filter((r) => {
+          const crew = driversCrewsMap[r.custId]
+          return (crew?.class || "-") === c.name
+        })
+        return acc
+      },
+      {} as Record<string, RankingItem[]>
+    )
+
+    return classes.map((c, i) => (
+      <Fragment key={i}>
+        <ClassHead className={c.name} competition={competition} />
+
+        {rankingPerClass[c.name].map((rankingItem, i) => (
           <RankingRow
             key={i}
             rankingItem={rankingItem}
             driver={driversMap[rankingItem.custId]}
             team={driversTeamsMap[rankingItem.custId]}
             eventGroups={competition.eventGroups || []}
-            overallBestTimesPerEventGroup={overallBestTimesPerEventGroup}
+            overallBestTimesPerEventGroup={
+              overallBestTimesPerEventGroup[c.name]
+            }
+            position={i + 1}
           />
         ))}
-      </tbody>
-    </table>
+      </Fragment>
+    ))
+  }
+
+  // Single class
+  else {
+    const overallBestTimesPerEventGroup: Record<string, number> = {}
+
+    ranking.forEach((driverRanking) => {
+      Object.entries(driverRanking.results).forEach(
+        ([eventGroupId, results]) => {
+          Object.values(results).forEach((result) => {
+            if (
+              result > 0 &&
+              (!overallBestTimesPerEventGroup[eventGroupId] ||
+                result < overallBestTimesPerEventGroup[eventGroupId])
+            ) {
+              overallBestTimesPerEventGroup[eventGroupId] = result
+            }
+          })
+        }
+      )
+    })
+
+    return ranking.map((rankingItem, i) => (
+      <RankingRow
+        key={i}
+        rankingItem={rankingItem}
+        driver={driversMap[rankingItem.custId]}
+        team={driversTeamsMap[rankingItem.custId]}
+        eventGroups={competition.eventGroups || []}
+        overallBestTimesPerEventGroup={overallBestTimesPerEventGroup}
+        position={i + 1}
+      />
+    ))
+  }
+}
+
+function ClassHead({
+  className,
+  competition,
+}: {
+  className: string
+  competition: Competition
+}) {
+  return (
+    <tr>
+      <td
+        colSpan={
+          4 +
+          (competition.eventGroups?.reduce(
+            (acc, eg) => acc + (eg.sessions?.length || 0),
+            0
+          ) || 0)
+        }
+        className="text-center font-bold bg-gray-900 text-gray-300 py-2 text-xl"
+      >
+        {className}
+      </td>
+    </tr>
   )
 }
 
@@ -116,6 +238,7 @@ function RankingRow({
   team,
   eventGroups,
   overallBestTimesPerEventGroup,
+  position,
 }: RankingRowProps) {
   if (!rankingItem) {
     return null
@@ -123,7 +246,7 @@ function RankingRow({
 
   return (
     <tr className="border-b border-gray-700 bg-gray-800">
-      <td className="px-6 py-4 text-center">P{rankingItem.position}</td>
+      <td className="px-6 py-4 text-center">P{position}</td>
 
       <td className="px-6 py-4">
         <div className="flex items-center space-x-2">
